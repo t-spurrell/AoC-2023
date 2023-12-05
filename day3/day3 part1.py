19d0ad52e4be3a0e3948b76797cfe261bb48fbc6
Trevor Spurrell <jaguarxman@gmail.com>
2023-12-04 23:14:36 -0500

    start of day3

---
+++ day3/day3 part1.py
@@ -0,0 +1,6 @@
+with open('day3/input.txt', 'r') as file:
+    data = file.read().splitlines()
+
+for row_index, row in enumerate(data):
+    for character_index, character in enumerate(row):
+        pass
